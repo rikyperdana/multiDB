5d7a9feb252a19475640385909816bc0cb341942
Riky Perdana <perdanariky@gmail.com>
2024-12-02 19:19:01 +0700

    redis ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const print = console.log,
 couch = require('nano'),
-redis = require('redis'),
+redis = require('redis').createClient(),
 maria = require('mariadb'),
 mongo = require('mongodb'),
 postgre = require('postgres'),
@@ -11,7 +11,7 @@ dbName = 'test', // universal dummy
 // 1.1. Mongo
 mongoURL = 'mongodb://localhost:27017',
 mongoDB = new mongo.MongoClient(mongoURL)
-mongoDB.connect()
+0 && mongoDB.connect()
 .then(client => print(
   client.db(dbName)
   .collection('users')
@@ -20,6 +20,18 @@ mongoDB.connect()
 ))
 
 // 1.2. Redis
+0 && redis.connect().then(
+  ok => redis.set(
+    'users', JSON.stringify({
+      username: 'riky',
+      password: 'asdfkj'
+    })
+  ).then(ok => [
+    redis.get('users')
+    .then(print)
+  ])
+)
+
 // 1.3. Couch
 // 1.4. Maria
 // 1.5. Postgre
